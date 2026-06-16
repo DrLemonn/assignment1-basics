b37b43becde35977a11a08ef537d6d11355767c7
yulinhan <yulinhan@meituan.com>
2026-06-16 10:37:43 +0800

    train bpe

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -2,6 +2,7 @@ import os
 from typing import BinaryIO
 import regex as re
 from collections import Counter,defaultdict
+from multiprocessing import Pool
 
 
 def find_chunk_boundaries(
@@ -50,6 +51,21 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
+
+def process_chunk(args):
+    chunk, special_tokens = args
+    chunk_pretoken_freq: Counter[tuple[bytes,...]] = Counter()
+
+    escaped_special_tokens = [re.escape(special_token) for special_token in special_tokens]
+    splited_chunks = re.split('|'.join(escaped_special_tokens), chunk)
+    for splited_chunk in splited_chunks:
+        for match in re.finditer(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""", splited_chunk):
+            pretoken_bytes = match.group().encode("utf-8")
+            key = tuple(pretoken_bytes[i:i+1] for i in range(len(pretoken_bytes)))
+            chunk_pretoken_freq[key] += 1
+    
+    return chunk_pretoken_freq
+
 def train_bpe(
     input_path: str | os.PathLike,
     vocab_size: int,
@@ -87,10 +103,11 @@ def train_bpe(
         vocab[len(vocab)] = bytes([i])
 
     with open(input_path, "rb") as f:
-        num_processes = 4
+        num_processes = 8
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
         pretoken_freq: Counter[tuple[bytes,...]] = Counter()
+        tasks = []
 
         # The following is a serial implementation, but you can parallelize this
         # by sending each start/end pair to a set of processes.
@@ -98,74 +115,70 @@ def train_bpe(
             f.seek(start)
             chunk = f.read(end - start).decode("utf-8", errors="ignore")
             # Run pre-tokenization on your chunk and store the counts for each pre-token
-            chunk_pretoken_freq: Counter[tuple[bytes,...]] = Counter()
+            tasks.append((chunk, special_tokens))
 
-            escaped_special_tokens = [re.escape(special_token) for special_token in special_tokens]
-            splited_chunks = re.split('|'.join(escaped_special_tokens), chunk)
-            for splited_chunk in splited_chunks:
-                for match in re.finditer(r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""", splited_chunk):
-                    pretoken_bytes = match.group().encode("utf-8")
-                    key = tuple(pretoken_bytes[i:i+1] for i in range(len(pretoken_bytes)))
-                    chunk_pretoken_freq[key] += 1
-            
-            pretoken_freq.update(chunk_pretoken_freq)
+        with Pool(processes=num_processes) as pool:
+            results = pool.map(process_chunk, tasks)
+
+        for result in results:
+            pretoken_freq.update(result)
         
         # 根据pre-token统计byte-pair的频率，课件中提到如果每一次merge之后都重新遍历所有的pre-token效率很低，其实对于每个
         # 被merge的bytes，只有包含它的pre-token会受到影响，可以建立一个byte-pair到pre-token的索引。
         # 比如对pre-token"abcde"，本来包含了"ab","bc","cd","de"-四个bytes-pair，如果bc被merge了，那就变成了"abc","bcd","de"-三个。
         # 统计出的bytes-pair "ab","bc"和"cd"就要减去pretoken出现的频率，"abc","bcd"需要增加。
         
-        bytes_pair_freq: Counter[tuple[bytes, bytes]] = Counter()
-        bytes_pair_to_pretokens: dict[tuple[bytes, bytes], set[tuple[bytes,...]]] = defaultdict(set)
+    bytes_pair_freq: Counter[tuple[bytes, bytes]] = Counter()
+    bytes_pair_to_pretokens: dict[tuple[bytes, bytes], set[tuple[bytes,...]]] = defaultdict(set)
 
-        for pretoken, freq in pretoken_freq.items():
-            for i in range(len(pretoken) - 1 ):
-                bytes_pair_freq[pretoken[i:i+2]] += freq
-                bytes_pair_to_pretokens[pretoken[i:i+2]].add(pretoken)
+    for pretoken, freq in pretoken_freq.items():
+        for i in range(len(pretoken) - 1 ):
+            bytes_pair_freq[pretoken[i:i+2]] += freq
+            bytes_pair_to_pretokens[pretoken[i:i+2]].add(pretoken)
+    
+    while len(vocab) < vocab_size:
+        max_freq = max(bytes_pair_freq.values())
+        to_merge = max([k for k,v in bytes_pair_freq.items() if v == max_freq])
+
+        merges.append(to_merge)
+        vocab[len(vocab)] = to_merge[0] + to_merge[1]
+
+        affected_pretokens = list(bytes_pair_to_pretokens[to_merge])
         
-        while len(vocab) < vocab_size:
-            max_freq = max(bytes_pair_freq.values())
-            to_merge = max([k for k,v in bytes_pair_freq.items() if v == max_freq])
-
-            merges.append(to_merge)
-            vocab[len(vocab)] = to_merge[0] + to_merge[1]
-
-            affected_pretokens = list(bytes_pair_to_pretokens[to_merge])
+        for affected_pretoken in affected_pretokens:
+            i = 0
+            new_pretoken = []
+            while i < len(affected_pretoken):
+                if affected_pretoken[i:i+2] == to_merge:
+                    new_pretoken.append(to_merge[0] + to_merge[1])
+                    i += 2
+                else:
+                    new_pretoken.append(affected_pretoken[i])
+                    i += 1
             
-            for affected_pretoken in affected_pretokens:
-                i = 0
-                new_pretoken = []
-                while i < len(affected_pretoken):
-                    if affected_pretoken[i:i+2] == to_merge:
-                        new_pretoken.append(to_merge[0] + to_merge[1])
-                        i += 2
-                    else:
-                        new_pretoken.append(affected_pretoken[i])
-                        i += 1
+            new_pretoken = tuple(new_pretoken)
+
+            pretoken_freq[new_pretoken] = pretoken_freq[affected_pretoken]
+            del pretoken_freq[affected_pretoken]
+
+            temp_freq = pretoken_freq[new_pretoken]
+            for i in range(len(affected_pretoken) - 1):
+                pair = affected_pretoken[i:i+2]
+
+                bytes_pair_freq[pair] -= temp_freq
+                if bytes_pair_freq[pair] == 0:
+                    del bytes_pair_freq[pair]
                 
-                new_pretoken = tuple(new_pretoken)
+                bytes_pair_to_pretokens[pair].discard(affected_pretoken)
+                if len(bytes_pair_to_pretokens[pair]) == 0:
+                    del bytes_pair_to_pretokens[pair]
 
-                pretoken_freq[new_pretoken] = pretoken_freq[affected_pretoken]
-                del pretoken_freq[affected_pretoken]
-
-                temp_freq = pretoken_freq[new_pretoken]
-                for i in range(len(affected_pretoken) - 1):
-                    pair = affected_pretoken[i:i+2]
-
-                    bytes_pair_freq[pair] -= temp_freq
-                    if bytes_pair_freq[pair] == 0:
-                        del bytes_pair_freq[pair]
-                    
-                    bytes_pair_to_pretokens[pair].discard(affected_pretoken)
-                    if len(bytes_pair_to_pretokens[pair]) == 0:
-                        del bytes_pair_to_pretokens[pair]
-
-                for i in range(len(new_pretoken) - 1):
-                    pair = new_pretoken[i:i+2]
-                    bytes_pair_freq[pair] += temp_freq    
-                    bytes_pair_to_pretokens[pair].add(new_pretoken)
-        
-        return vocab, merges
+            for i in range(len(new_pretoken) - 1):
+                pair = new_pretoken[i:i+2]
+                bytes_pair_freq[pair] += temp_freq    
+                bytes_pair_to_pretokens[pair].add(new_pretoken)
+    
+    return vocab, merges
 
 
 if __name__ == "__main__":
